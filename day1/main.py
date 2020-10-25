a6d7126d9ac966b33c2e1d0c87fcc382e456f72d
wzhy0576 <2406795465@qq.com>
2020-10-25 15:06:49 +0800

    Signed-off-by: wzhy0576 <2406795465@qq.com>

---
+++ day1/main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from layout import *
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
